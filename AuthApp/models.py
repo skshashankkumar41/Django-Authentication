698dc7d13115521839f8a1502e43f6c4ccb74f87
Shashank Prasad <skshashankkumar41@gmail.com>
2020-10-30 00:28:37 +0530

    created model for registration

--- AuthApp/models.py
+++ AuthApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Register(models.Model):
+    userid = models.AutoField(primary_key=True,default=1)
+    name = models.CharField(max_length=200,null=True)
+    username = models.CharField(max_length=200,null=True)
+    password = models.CharField(max_length=200,null=True)
+    mobile = models.CharField(max_length=10,null=True)
+    email = models.EmailField()
+    date_created = models.DateTimeField(auto_now_add=True)
