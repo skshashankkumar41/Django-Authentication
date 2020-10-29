484c5f045a112fe21834c0a6f7b1931b00be1376
Shashank Prasad <skshashankkumar41@gmail.com>
2020-10-30 01:10:22 +0530

    updated autofield

--- AuthApp/models.py
+++ AuthApp/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Register(models.Model):
-    userid = models.AutoField(primary_key=True,default=1)
+    userid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=200,null=True)
     username = models.CharField(max_length=200,null=True)
     password = models.CharField(max_length=200,null=True)
